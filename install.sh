0212f197d618a1f1d403c043b2b5dee0723eb63c
Nico Suria <nicosuria@gmail.com>
2019-09-19 17:09:37 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ mkdir ~/.vim/bundle
 mkdir ~/.vim/autoload
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cd ~/.vim/bundle
-git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle
+cd ~/.vim/bundle ; git clone git://github.com/altercation/vim-colors-solarized.git
 curl https://raw.githubusercontent.com/nicosuria/vimrc/master/.vimrc --output ~/.vimrc
