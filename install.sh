f9ca15c07f0f67f0f6fe26895ff73b1ef730a848
Nico Suria <nico.suria@shopify.com>
2021-10-20 10:34:41 -0700

    Fix typo

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo apt-get install vim 
 mkdir -p ~/.vim/bundle ~/.vim/autoload && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-Ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
